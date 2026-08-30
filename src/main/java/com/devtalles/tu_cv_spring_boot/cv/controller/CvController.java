52e9a517a72ea9409052856edae148ab16d6f43a
Aldahir Alonso <aldahir0603@hotmail.com>
2026-08-30 15:43:44 -0600

    Creando Controlador y metodos Get para vistas index y pep.

---
+++ src/main/java/com/devtalles/tu_cv_spring_boot/cv/controller/CvController.java
@@ -0,0 +1,19 @@
+package com.devtalles.tu_cv_spring_boot.cv.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CvController {
+
+    @GetMapping("/home")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/pep")
+    public String pep() {
+        return "pep";
+    }
+
+}
