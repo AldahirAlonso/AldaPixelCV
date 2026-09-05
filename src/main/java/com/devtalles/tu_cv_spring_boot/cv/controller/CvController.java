99d7589fe2ae2f1eccf47538d0b523cf68396107
Aldahir Alonso <aldahir0603@hotmail.com>
2026-09-05 14:17:18 -0600

    Definiendo ruta principal cv con RequestMaping.

--- src/main/java/com/devtalles/tu_cv_spring_boot/cv/controller/CvController.java
+++ src/main/java/com/devtalles/tu_cv_spring_boot/cv/controller/CvController.java
@@ -2,8 +2,10 @@ package com.devtalles.tu_cv_spring_boot.cv.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/cv")
 public class CvController {
 
     @GetMapping("/home")
@@ -11,7 +13,7 @@ public class CvController {
         return "index";
     }
 
-    @GetMapping("/pep")
+    @GetMapping({"/pep", "", "/home"})
     public String pep() {
         return "pep";
     }
