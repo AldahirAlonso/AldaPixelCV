7bb0b7c918d736095ff3bc6ccd94a8e62b1c0748
Aldahir Alonso <aldahir0603@hotmail.com>
2026-09-05 16:18:20 -0600

    Creando Controlador para mostrar skills.

---
+++ src/main/java/com/devtalles/tu_cv_spring_boot/cv/controller/SkillsController.java
@@ -0,0 +1,22 @@
+package com.devtalles.tu_cv_spring_boot.cv.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class SkillsController {
+
+    @GetMapping("/skills")
+    public String skills(Model model) {
+        List<String> skills = List.of("Java", "Spring Boot",
+                "JavaScript", "HTML", "CSS");
+        List<String> list = new ArrayList<>();
+        model.addAttribute("skills", list);
+
+        return "skills";
+    }
+}
