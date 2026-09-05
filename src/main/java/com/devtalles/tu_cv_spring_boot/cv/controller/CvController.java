9d9d9e17fb5101f2a94094e68c00161c33ea2e89
Aldahir Alonso <aldahir0603@hotmail.com>
2026-09-05 15:15:21 -0600

    Uso de clase ModelMap para tener vistas dinamicas.

--- src/main/java/com/devtalles/tu_cv_spring_boot/cv/controller/CvController.java
+++ src/main/java/com/devtalles/tu_cv_spring_boot/cv/controller/CvController.java
@@ -1,6 +1,9 @@
 package com.devtalles.tu_cv_spring_boot.cv.controller;
 
+import com.devtalles.tu_cv_spring_boot.cv.model.Person;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,14 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/cv")
 public class CvController {
 
-    @GetMapping("/home")
-    public String index() {
+    @GetMapping({"/index", "", "/"})
+    public String index(ModelMap model) {
+        Person person = new Person("Aldahir", "Alonso", "Software Engineer");
+        model.addAttribute("name", "Aldahir");
+        model.addAttribute("persona", person);
         return "index";
     }
-
-    @GetMapping({"/pep", "", "/home"})
-    public String pep() {
-        return "pep";
-    }
-
+    
 }
